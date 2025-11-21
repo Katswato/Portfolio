b7a12711178fb8ea1552bed0978090b87074a514
Katswato <ujicofluff@gmail.com>
2025-11-20 23:05:58 -0800

    [Feature] Added Profile component to the Home component.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Profile } from "../src/profile";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  //return <Welcome />;
+  return (
+    <div>
+	  <Profile />
+	</div>
+  );
 }
