76dc06169bd16fb3c66fe7c70c1dfc168147bb56
Katswato <ujicofluff@gmail.com>
2025-11-27 16:03:52 -0800

    [Feature] Added ability to activate section boxes and change their colors. Used useState hook to allow changing of classes upon being clicked.

--- app/src/sectionBox.tsx
+++ app/src/sectionBox.tsx
@@ -1,11 +1,16 @@
 import "./sectionBox.css";
 
+import React from "react";
+import { useState } from "react";
+
 export function SectionBox({sectionName, className})
 {
+  const [isActive, setIsActive] = useState(false);
+  const determinedClassName = isActive ? "default-section-box-active" : "default-section-box";
   return (
     <div>
-	  <button onClick={()=>{console.log("SectionBoxLog");}}>
-	    <div className={className}>
+	  <button onClick={()=>{setIsActive(!isActive)}}>
+	    <div className={determinedClassName}>
 	      {sectionName}
 	    </div>
 	  </button>
