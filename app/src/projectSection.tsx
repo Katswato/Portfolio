5d1d858b091a413455600b57ae198b88002c5dae
Katswato <ujicofluff@gmail.com>
2025-11-27 16:02:51 -0800

    [Refactor] Added a new grid box to contain the date of the project. Edited the grid sizes of the projectSection component so that it looked more centered.

--- app/src/projectSection.tsx
+++ app/src/projectSection.tsx
@@ -1,20 +1,24 @@
 import "./projectSection.css";
 
-export function ProjectSection({name, description, youtubeLink, githubLink, downloadLink})
+
+export function ProjectSection({name, description, date, youtubeLink, githubLink, downloadLink})
 {
   return(
-    <div>
+    <div styles={{ "display":"flex", "justify-content":"center", "align-items":"center"}}>
 	  <div className="project-section">
 	    <div className="project-box-1">
 	      <ProjectName name={name}/>
 		</div>
-	    <div className="project-box-2">
-	      <ProjectYouTubeLink youtubeLink={youtubeLink}/>
+		<div className="project-box-2">
+		  <ProjectData date={date}/>
 		</div>
-		<div className="project-box-3">
+        <div className="project-box-3">
 	      <ProjectDescription description={description}/>
 		  <ProjectGitHubLink githubLink={githubLink}/>
 	      <ProjectDownloadLink downloadLink={downloadLink}/>
+		</div>
+	    <div className="project-box-4">
+	      <ProjectYouTubeLink youtubeLink={youtubeLink}/>
 		</div>
 	  </div>
 	</div>
@@ -26,6 +30,15 @@ export function ProjectName({name})
   return(
     <div className="project-name">
       {name}
+	</div>
+  );
+}
+
+export function ProjectData({date})
+{
+  return(
+    <div className="project-date">
+      {date}
 	</div>
   );
 }
