e1f6182a1d28e1314eaea7668d8e4282a1e81ab4
Katswato <ujicofluff@gmail.com>
2026-01-06 03:48:51 -0800

    [Feature] Removed data related interfaces and variables and transferred them into a different file. Transitioned from generating divs into an array to mapping data from an array into a div. Added state hooks, corrected useEffect hook, and modified component variables so that they are properly updated when tags are changed.

--- app/src/sectionContent.tsx
+++ app/src/sectionContent.tsx
@@ -1,244 +1,71 @@
 import { ProjectSection } from "./projectSection";
 import { SectionBox } from "./sectionBox";
-import React from "react";
 import { 
   useState,
-  useEffect
+  useEffect,
 } from "react";
+import {
+  isArrayContentsEqual,
+  isArraySubset,
+  isArrayPartial,
+} from "./util";
+import { 
+  type SectionBoxData,
+  type ProjectSectionData,
+  allTags,
+  PersonalSectionBoxDataCollection,
+  PersonalProjectSectionDataCollection
+} from "./data";
+import "./sectionContent.css"
 
-import smartGlassesVideo from "./../videos/smartGlasses.mp4"
-import totalKneeReplacementVideo from "./../videos/totalKneeReplacement.mp4"
-
-interface SectionBoxData {
-  name: string;
-  className: string;
-  tags: string[];
-}
-
-interface ProjectSectionData {
-  name: string;
-  description: string;
-  youtubeLink?: string;
-  tags: string[];
-}
-
-interface SectionBoxDataCollection {
-  collection: SectionBoxData[];
-}
-
-interface ProjectSectionDataCollection {
-  collection: ProjectSectionData[];
-}
-
-
-const PersonalSectionBoxDataCollection : SectionBoxDataCollection = {
-  collection: [
-    {  
-	  name: "All", 
-	  className: "default-section-box", 
-	  tag: "All"
-	},
-    {
-	  name: "Game Projects", 
-	  className: "default-section-box", 
-	  tag: "Game"
-	},
-    {
-	  name: "Embedded System Projects", 
-	  className: "default-section-box", 
-	  tag: "Embedded"
-	},
-    {
-	  name: "Web Projects", 
-	  className: "default-section-box", 
-	  tag: "Web"
-	}
-  ]
-  
-};
-
-const generateSectionBoxDataDivs = (collection: SectionBoxData[], tags, setTags) => {
-  let length = collection.length;
-  let resultDivs = [];
-  for (let i = 0; i < length; ++i) {
-    let data = collection[i];
-	let isActivated = false;
-	if (tags?.includes(data.tag)) {
-	  isActivated = true;
-	}
-    resultDivs.push({
-	  id: i, 
-	  div: <SectionBox
-	    sectionName={data.name}
-		isActive={isActivated}
-		tag={data.tag}
-		tags={tags}
-		setTags={setTags}
-	  />
-	});
-  }
-  return (
-    <div style={{display: "flex", justifyContent: "center"}}>
-      { 
-	    resultDivs.map(item => (
-	      <div key={item.id}>
-	        {item.div}
-	      </div>
-	    ))
-	  }
-	</div>
-  )
-}
-
-const hasTags = (tags: string[], heldTags: string[]) => {
-  let length = heldTags.length;
-  for (let i = 0; i < length; ++i) {
-    if (tags.includes(heldTags[i])) {
-      return true;
-    }
-  }
-  return false;
-}
-
-const getWithTags = (unfilteredCollection: ProjectSectionData[], heldTags: string[]) => {
-  let filteredCollection = unfilteredCollection.filter(data => hasTags(data.tags, heldTags));
-
-  return filteredCollection;
-}
-
-const generateProjectSectionDataDivs = (unfilteredCollection: ProjectSectionDataCollection, tags: string[], setTags) => {
-  let filteredCollection: ProjectSectionData[] = getWithTags(unfilteredCollection, tags);
-  let filteredCollectionLength: int = filteredCollection.length;
-
-  let resultDivs = [];
-  for (let i = 0; i < filteredCollectionLength; ++i)
-  {
-    let data = filteredCollection[i];
-    resultDivs.push(
-	  {
-	    id: i,
-        div: <ProjectSection
-  	      name={data.name}
-  	      description={data.description}
-		  date={data.date}
-  	      youtubeLink={data.youtubeLink}
-		  video={data.video}
-		  videoType={data.videoType}
-  	    />
-	  }
-    );
-  }
-
-  return (
-    <div>
-    { 
-	  resultDivs.map(item => (
-        <div key={item.id}>
-          {item.div}
-  	    </div>
-	  ))
-	}
-	</div>
-  );
-}
-
-const PersonalProjectSectionDataCollection : ProjectSectionDataCollection = {
-  collection: [
-    {
-	  name: "WarSim", 
-	  description: 
-	    "A RTS game that allows you to control hand-programmed behavioural AI units to defeat your enemies. \
-		 In this game, I wanted to simulate military technology. As I was prototyping, I found myself dissatisfied \
-		 with Unity's navigation mesh when creating flying units so I created my own behavioural system for aircraft. \
-		 This game was built using C# programming and utilizes parallelism to increase performance. \
-		",
-	  date: "2025 October - Present",
-	  youtubeLink: "https://www.youtube.com/embed/iC2aBLneca8",
-	  tags: ["All", "Game"]
-	},
-	{
-	  name: "Total Knee Replacement", 
-	  description: 
-	    "A singleplayer 3D game that tests the user's skill in performing knee surgery. I wanted to create a game that was \
-		 in the field of healthcare and followed a Halloween theme since it was that time of year. The game was built in gamejam-like \
-		 like conditions. This game was built in Unity with the C# programming language. \
-		",
-	  date: "2025 November",
-	  youtubeLink: "https://www.youtube.com/embed/plvDetf4lO4",
-	  tags: ["All", "Game"]
-	},
-    {
-	  name: "Smart Glasses", 
-	  description: 
-	    " \
-         A device that uses multiple trained AI models to interpret live audio and translate it into text, \
-		 translate American Sign Language, and detect color to help Americans with disabilities. This project was made with \
-		 in collaboration with 3 other students in our embedded systems class. This device was built using a raspberry pi, \
-		 an led screen, camera, microphone, and the implementation of several pytorch trained AI models. \
-		",
-	  date: "2025 January - 2025 March",
-	  video: smartGlassesVideo,
-	  videoType: "video/mp4",
-	  tags: ["All", "Embedded"]
-	},
-	{
-	  name: "Rhythm Aimer",
-	  description: 
-	    "Rhythm Aimer is a 1-2 player rhythm game built to challenge the timing and aim of players. When coming up with a project in my \
-		embedded systems class, I wanted to make a game similar to osu! because I'm a big fan of the game. osu! is a rhythm \
-		game where you aim for the circles and click them to the rhythm. In Rhythm Aimer, you aim for circles vertically and \
-		use a button to click. This game was built using arduino, led matrices, various electrical components, and C AVR libraries. \
-		",
-	  date: "2024 April - 2024 May",
-	  youtubeLink: "https://www.youtube.com/embed/edhRERpTr1E",
-	  tags: ["All", "Game", "Embedded"]
-	},
-    {
-	  name: "Chopping Game", 
-	  description: 
-	    "Chopping Game is a 2D singleplayer game where the goal is to cut down a bean stalk without getting hit by obstacles. \
-		 I wanted to make a game that was similar to a minigame I saw one of my friends playing. I built a similar game \
-		 with some creative twists. This game was built in Unity with the C# programming language. \
-		",
-	  date: "2025 November",
-	  youtubeLink: "https://www.youtube.com/embed/-FYqe8N6vAg",
-	  tags: ["All", "Game"]
-	},
-    {
-	  name: "Goblino Mode", 
-	  description: 
-	    "Goblino Mode is a 2D singleplay game where the goal is to defeat a boss without dying. Goblino Mode is my first game made \
-		 in Unity. I planned on making a metroidvania game with Terraria-like mechanics but I ended \
-		 up making a single incomplete boss fight. This game was built in Unity with the C# programming language. \
-		",
-	  date: "2025 November",
-	  youtubeLink: "https://www.youtube.com/embed/I3CVhHuohG8",
-	  tags: ["All", "Game"]
-	}
-  ]
-};
-
-export function SectionContent({})
-{
+export function SectionContent({}) {
   console.log("SectionContent rendered.");
-  const [tags, setTags] = useState(["All"]);
-
-  console.log("Before Tags: ", tags);
-  let sectionDivs = generateSectionBoxDataDivs(PersonalSectionBoxDataCollection.collection, tags, setTags);
-  let projectDivs = generateProjectSectionDataDivs(PersonalProjectSectionDataCollection.collection, tags, setTags);
-  console.log("After Tags: ", tags);
+  const [tags, setTags] = useState(allTags);
+  const [allActive, setAllActive] = useState(true);
+  const [sections, setSections] = useState(PersonalSectionBoxDataCollection.collection);
+  const [projects, setProjects] = useState(PersonalProjectSectionDataCollection.collection);
 
   useEffect(() => {
-    console.log("PostUseEffect Tags: ", tags);
-    projectDivs = generateProjectSectionDataDivs(PersonalProjectSectionDataCollection.collection, tags, setTags);
+    setAllActive(isArrayContentsEqual(tags, allTags));
+    //console.log("[SectionContent] tags: ", tags);
+    //console.log("[SectionContent] allActive: ", isArrayContentsEqual(tags, allTags))
   }, [tags]);
 
   return(
-    <div>
-	  <div style={{display: "flex", "justify-content": "center"}}>
-	    {sectionDivs}
+    <div id="section-page">
+	  <div id="section-header">
+	    {sections.length == 0 
+          ? <div></div>
+          : sections.map((data: SectionBoxData) => (
+            <SectionBox
+              key={data.name}
+              sectionName={data.name}
+              allActive={allActive}
+              isActive={(data.name == "All" || !allActive) && isArraySubset(tags, data.tags)}
+              sectionTags={data.tags}
+              selectedTags={tags}
+              setTags={setTags}
+            />    
+          ))
+        }
 	  </div>
-	  {projectDivs}
+      <div id="section-projects">
+	    {projects.length == 0 
+          ? <div></div>
+          : projects.map((data: ProjectSectionData) => (
+            <ProjectSection
+              key={data.name}
+              name={data.name}
+              description={data.description}
+              date={data.date}
+              youtubeData={data.youtubeData}
+              videoData={data.videoData}
+              tags={data.tags}
+              isActive={isArrayPartial(tags, data.tags)}
+            />
+          ))
+        }
+      </div>
 	</div>
   )
 }
